ac5e3fd90b4b74760807404a9b618d2af78ce7c1
Brandon Wood <brw422@g.harvard.edu>
2024-04-14 10:03:34 -0400

    got zoom working, now going to rework how the scenes function, big change to stop any future rework.

--- classes/level.gd
+++ classes/level.gd
@@ -1,9 +1,9 @@
-extends Node
+extends Node3D
 class_name level
 
 @export var rotationAmount = 45
 @export var zoomAmount = 1.8
-@export var zoom_mouse_sen = 0.2
+@export var zoomed_mouse_sen = 0.2
 @export var target = 1
 @export var nextLevelPath = ""
 
@@ -17,6 +17,7 @@ var zoomed = false
 
 func _ready():
 	get_tree().paused = false
+	$AudioStreamPlayer.loop = true
 	person_handler.person_events.connect(handle_person_event)
 	$Summons.Sprite.frame = target
 	started = true
@@ -36,24 +37,45 @@ func win():
 	game_summary_menu.display()
 
 func _input(event):
-	if zoomed and event is InputEventMouseMotion:
-		var camera_anglev = 0
-		camera.rotate_y(deg_to_rad(-event.relative.x*zoom_mouse_sen))
-		var changev = -event.relative.y * zoom_mouse_sen
-		camera_anglev += changev
-		camera.rotate_x(deg_to_rad(changev))
+#	if zoomed and event is InputEventMouseMotion:
+#		camera.rotate_y(deg_to_rad(-event.relative.x * zoomed_mouse_sen))
+#		var mousePos = get_mouse_coordinates()
+#		if mousePos.size() > 0:
+#			$CameraPivot.position.x = mousePos.position.x
+#			$CameraPivot.position.y = mousePos.position.y
 	if event.is_action_pressed("rotate_left"):
 		rotate_left()
 	if event.is_action_pressed("rotate_right"):
 		rotate_right()		
 	if event.is_action_pressed("zoom") and started:
 		if zoomed:
-			camera.position.z = 0
-			camera.rotation = Vector3(0,0,0)
+			$CameraPivot.position = Vector3.ZERO
+			camera.position = Vector3.ZERO
+			camera.rotation = Vector3.ZERO
 			zoomed = false
 		else:
-			camera.position.z = -zoomAmount
+			var mousePos = get_mouse_coordinates()
+			print_debug(mousePos, zoomed)
 			zoomed = true
+			if mousePos.size() > 0:
+				camera.position.z = -zoomAmount
+				$CameraPivot.position.x = mousePos.position.x
+				$CameraPivot.position.y = mousePos.position.y
+
+
+func get_mouse_coordinates():
+	var space_state = get_world_3d().direct_space_state
+	var cam = camera
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * 100
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	# This is the collision of the ground only.
+	query.collision_mask = 2
+	return space_state.intersect_ray(query)
+
 
 func rotate_left():
 	$CameraPivot.rotate_y(deg_to_rad(rotationAmount))
